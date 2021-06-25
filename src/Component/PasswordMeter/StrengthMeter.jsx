b1010238eae6c2724f0ef3d84170e69bd0954f13
Ashish <abhagat629@gmail.com>
2021-06-25 22:34:42 +0530

    added login page

--- src/Component/PasswordMeter/StrengthMeter.jsx
+++ src/Component/PasswordMeter/StrengthMeter.jsx
@@ -4,6 +4,7 @@ import styles from "./Meter.module.css"
 const StrengthMeter = ({count = 1}) => {
     return (
         <div className = {styles.main}>
+            <p>password strength</p>
             {
                 Array.from({length: 5}).map((e, i) => (
                     <div style = {i < count? {background: "green"}: null} key = {i}></div>
--- src/Pages/Login/Index.jsx
+++ src/Pages/Login/Index.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Input from "../../Component/Input/Index";
 import { AiFillEye } from "react-icons/ai";
 import Button from "../../Component/Button/Button";
+import styles from "./Index.module.css";
 
 const initState = {
     username: "",
@@ -13,27 +14,46 @@ const LogIn = () => {
     const [userDatails, setUserDatails] = useState(initState);
 
     const handleChange = (e) => {
-        const {name, value} = e.target
-        console.log(name, value)
+        const { name, value } = e.target;
+        console.log(name, value);
     };
 
-    const handleClick = () => {
-
-    }
+    const handleClick = () => {};
 
     return (
-        <div>
-            <Input
-                handleChange={handleChange}
-                type= "text"
-                name="username"
-                placeholder="username"
-                regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})"
-                icon = {<AiFillEye />}
-            />
-             <Button name = {"Button"} handleClick = {handleClick}/>
+        <div className={styles.main}>
+            <div>
+                <div className={styles.form_wrapper}>
+                    <div>
+                        <h1>Login</h1>
+                        <p>Enter your details below to continue</p>
+                    </div>
+                    <Input
+                        handleChange={handleChange}
+                        type="email"
+                        name="email"
+                        placeholder="Email"
+                        regex="^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})"
+                        icon={<AiFillEye />}
+                    />
+                    <Input
+                        handleChange={handleChange}
+                        type="text"
+                        name="username"
+                        placeholder="username"
+                        regex="^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})"
+                        icon={<AiFillEye />}
+                    />
+                    <div className={styles.buttons}>
+                        <Button name={"Login"} handleClick={handleClick} />
+                    </div>
+                    <p>Dont have and account? <span style = {{color: "rgb(204, 181, 53)"}}>Sign up here</span></p>
+                </div>
+            </div>
+            <div>
+                <img src="http://htc-wallpaper.com/wp-content/uploads/2015/06/Star-Wars-Logo.jpg" alt="logo"/>
+            </div>
         </div>
-       
     );
 };
 
